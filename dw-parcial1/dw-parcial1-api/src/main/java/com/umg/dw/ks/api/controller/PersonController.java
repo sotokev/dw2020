a24cf2486d07a39b8476448c76405180d44ce3f2
Kevin Soto <uni.kevin.es@gmail.com>
2020-11-12 02:00:26 -0600

    WebFlux  desde api

--- dw-parcial1/dw-parcial1-api/src/main/java/com/umg/dw/ks/api/controller/PersonController.java
+++ dw-parcial1/dw-parcial1-api/src/main/java/com/umg/dw/ks/api/controller/PersonController.java
@@ -2,13 +2,19 @@ package com.umg.dw.ks.api.controller;
 
 import com.umg.dw.ks.api.repository.PersonRepository;
 import com.umg.dw.ks.core.entities.Person;
-import com.umg.dw.ks.core.entities.Product;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Api(value = "/", description = "REST Product")
 @RestController
@@ -17,6 +23,13 @@ public class PersonController {
 
     @Autowired
     PersonRepository personRepository;
+
+    private EmitterProcessor<Person> notificationProcessor;
+
+    @PostConstruct
+    private void createProcessor(){
+        notificationProcessor = EmitterProcessor.create();
+    }
 
     @CrossOrigin
     @RequestMapping(
@@ -39,6 +52,10 @@ public class PersonController {
             produces = "application/json")
     public Person create(@RequestBody Person persona) {
         persona = personRepository.save(persona);
+
+        System.out.println("Notificando nueva persona:"+persona.getName());
+        notificationProcessor.onNext(persona);
+
         return persona;
     }
 
@@ -58,5 +75,49 @@ public class PersonController {
             produces = "application/json")
     public void delete(@PathVariable("id") int id) {
         personRepository.deleteById(id);
+    }
+
+    private Flux<ServerSentEvent<Person>> getPersonaSSE() {
+
+        // SSE
+        // notification processor retorna un FLUX en el cual podemos estar "suscritos" cuando este tenga otro valor ...
+        return notificationProcessor
+                .log()
+                .map(
+                        (persona) -> {
+                            System.out.println("Sending Persona:" + persona.getId());
+                            return ServerSentEvent.<Person>builder()
+                                    .id(UUID.randomUUID().toString())
+                                    .event("persona-result")
+                                    .data(persona)
+                                    .build();
+                        }
+                )
+                .concatWith(Flux.never());
+    }
+
+    private Flux<ServerSentEvent<Person>> getNotificationHeartbeat() {
+        return Flux.interval(Duration.ofSeconds(15))
+                .map(i -> {
+                    System.out.println(String.format("sending heartbeat [%s] ...", i.toString()));
+                    return ServerSentEvent.<Person>builder()
+                            .id(String.valueOf(i))
+                            .event("heartbeat-result")
+                            .data(null)
+                            .build();
+                });
+    }
+
+    @GetMapping(
+            value = "/notification/sse"
+    )
+    public Flux<ServerSentEvent<Person>> getJobResultNotification() {
+
+        // enviar dos flujos al mismo tiempo
+        return Flux.merge(
+                getNotificationHeartbeat(),
+                getPersonaSSE()
+        );
+
     }
 }
